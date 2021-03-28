e2b30b15a61ad808da8faa61942f2067ef9aaac5
Benjamin Ramser <legionaerr@googlemail.com>
2021-03-28 17:18:20 +0200

    refactor: return json response if Command.SET instead of dict

--- pyle38/commands/executable.py
+++ pyle38/commands/executable.py
@@ -1,8 +1,10 @@
-from typing import Any
 from typing import Dict
 from typing import Literal
+from typing import Union
 
+from ..client import Command
 from ..client import CommandArgs
+from ..responses import JSONResponse
 from .withclient import WithClient
 
 Compiled = Literal[str, CommandArgs]
@@ -12,6 +14,10 @@ class Executable(WithClient):
     def compile(self) -> Compiled:
         raise NotImplementedError("Not implemented")
 
-    async def exec(self) -> Dict[str, Any]:
+    async def exec(self) -> Union[JSONResponse, Dict]:
+        compiled_command = self.compile()
+        if compiled_command[0] == Command.SET:
 
-        return await self.client.command(*self.compile())
+            return JSONResponse(**(await self.client.command(*compiled_command)))
+
+        return await self.client.command(*compiled_command)
