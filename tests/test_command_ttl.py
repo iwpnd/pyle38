01e24d56debaae4034fe7b61e5fa8651e4c8c22a
Benjamin Ramser <legionaerr@googlemail.com>
2022-12-04 16:47:46 +0100

    test: 🚨 update within tests with random data

--- tests/test_command_ttl.py
+++ tests/test_command_ttl.py
@@ -1,15 +1,17 @@
 import pytest
 
-key = "fleet"
-id = "truck"
-expire = 5
+from .helper.random_data import random_string
 
 
 @pytest.mark.asyncio
 async def test_command_ttl(tile38):
-    response = await tile38.set(key, id).ex(5).point(1, 1).exec()
+    key = random_string()
+    id = random_string()
+    expire_in = 5
+
+    response = await tile38.set(key, id).ex(expire_in).point(1, 1).exec()
     assert response.ok
 
     response = await tile38.ttl(key, id)
     assert response.ok
-    assert response.ttl < expire
+    assert response.ttl < expire_in
